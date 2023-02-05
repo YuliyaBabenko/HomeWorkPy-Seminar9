84c7e52f18473d7785b1fa0f758a47f7742942a6
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-02-05 19:07:21 +0000

    without token num

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bot_commands import *
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-app = ApplicationBuilder().token("5972795135:AAGG4gLu7GBnOF2xSAeiqm6mvYpx4LkNwS4").build()
+app = ApplicationBuilder().token("TOKEN").build()
 
 app.add_handler(CommandHandler('hi', hi_command))
 app.add_handler(CommandHandler('time', time_command))
